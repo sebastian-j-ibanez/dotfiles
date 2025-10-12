1f6134a4f7bef227ca005e830e02f649c33276ee
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-10-11 23:29:53 -0400

    chore: clean up setup.sh script, add dependencies

--- setup.sh
+++ setup.sh
@@ -1,11 +1,39 @@
 #! /usr/bin/bash
 
-# Download go-git-prompt
-bin="$HOME/.local/bin"
-mkdir -p $bin 
-curl -Lo $bin/go-git-prompt https://github.com/sebastian-j-ibanez/go-git-prompt/releases/download/v0.2.0/go-git-prompt-v0.2-linux-amd64 
-chmod +x $bin/go-git-prompt
+echo "| Starting setup..."
 
+# Download go-git-prompt
+echo "| Installing go-git-prompt..."
+bin="$HOME/.local/bin"
+mkdir -p "$bin" 
+curl -Lo "$bin"/go-git-prompt https://github.com/sebastian-j-ibanez/go-git-prompt/releases/download/v0.2.0/go-git-prompt-v0.2-linux-amd64 
+chmod +x "$bin"/go-git-prompt
+echo "✓ Install successful"
 
 # Download dependencies
-sudo pacman -S git ghostty stow ttf-firacode-nerd zsh 
+echo "| Installing pacman packages..."
+dependencies=(
+  "chromium"
+  "git"
+  "github-cli"
+  "ghostty"
+  "go"
+  "gopls"
+  "helix"
+  "stow"
+  "ttf-firacode-nerd"
+  "vesktop"
+  "yay"
+  "zig"
+  "zls"
+)
+sudo pacman -Sy "${dependencies[@]}"
+echo "✓ Packages installed successfully."
+
+echo "| Installing AUR packages..."
+aur_dependencies=(
+  "visual-studio-code-bin"
+)
+yay -Sy "${aur_dependencies[@]}"
+echo "✓ Packages installed successfully."
+
