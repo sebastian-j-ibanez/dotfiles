6d520f6446f81fd7eb8de9ac2e97f6fbe7e04d4c
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-10-12 22:12:55 -0400

    fix: update setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 #! /usr/bin/bash
 
-echo "| Starting setup..."
+echo "  Starting setup..."
 
 # Download go-git-prompt
-echo "| Installing go-git-prompt..."
+echo "  Installing go-git-prompt..."
 bin="$HOME/.local/bin"
 mkdir -p "$bin" 
 curl -Lo "$bin"/go-git-prompt https://github.com/sebastian-j-ibanez/go-git-prompt/releases/download/v0.2.0/go-git-prompt-v0.2-linux-amd64 
 chmod +x "$bin"/go-git-prompt
-echo "✓ Install successful"
+echo "✓ Install successful."
 
 # Download dependencies
-echo "| Installing pacman packages..."
+echo "  Installing pacman packages..."
 dependencies=(
   "chromium"
+  "docker"
+  "docker-compose"
+  "ducker"
   "git"
   "github-cli"
   "ghostty"
@@ -28,12 +31,13 @@ dependencies=(
   "zls"
 )
 sudo pacman -Sy "${dependencies[@]}"
-echo "✓ Packages installed successfully."
+echo "✓ Install successful."
 
-echo "| Installing AUR packages..."
+echo "  Installing AUR packages..."
 aur_dependencies=(
   "visual-studio-code-bin"
 )
 yay -Sy "${aur_dependencies[@]}"
-echo "✓ Packages installed successfully."
+echo "✓ Install successful."
 
+echo "  Setup complete!"
